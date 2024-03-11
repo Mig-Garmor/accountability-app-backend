8e1c53c1e2122efec6245cef1a0ca7170f436989
Mig-Garmor <thecodermiguel@gmail.com>
2024-03-11 14:58:43 +0100

    Initial setup

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            // Create a new token for the user
+            $token = $user->createToken('YourAppNameTokenName')->plainTextToken;
+
+            return response()->json(['message' => 'Login successful', 'token' => $token]);
+        }
+
+        return response()->json(['error' => 'The provided credentials do not match our records.'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(['message' => 'Logout successful']);
+    }
+}
