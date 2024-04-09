79bdd5f0225050a5772d00a951cf9dcf6b4ed770
Mig-Garmor <thecodermiguel@gmail.com>
2024-04-09 19:55:02 +0200

    Group User Permissions

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -37,17 +37,26 @@ class GroupController extends Controller
 
     public function getActiveChallenge(Request $request, $groupId)
     {
-        // Retrieve the group with all its challenges
-        $group = Group::with(['challenges.users.tasks.completedTasks'])->find($groupId);
+        // Retrieve the current user's ID
+        $userId = auth()->id();
+
+        // Retrieve the group with challenges and also load the pivot data (permissions) for the authenticated user
+        $group = Group::with(['challenges.users.tasks.completedTasks', 'users' => function ($query) use ($userId) {
+            $query->where('users.id', $userId)->withPivot('permission');
+        }])->find($groupId);
 
         // Check if the group was found
         if (!$group) {
             return response()->json(['message' => 'Group not found'], 404);
         }
 
-        // Retrieve the current user's ID
-        // Assuming you are using Laravel's default authentication
-        $userId = auth()->id();
+        // Access the user's permission for this group from the pivot
+        $userPermission = optional($group->users->first())->pivot->permission ?? null;
+
+        // If for some reason we don't have a permission (user not part of the group), you might handle it differently
+        if (!$userPermission) {
+            return response()->json(['message' => 'User does not have permissions for this group'], 403);
+        }
 
         // Filter through the group's challenges to find the first one associated with the user
         $activeChallenge = $group->challenges->first(function ($challenge) use ($userId) {
@@ -59,7 +68,10 @@ class GroupController extends Controller
             return response()->json(['message' => 'There are no active challenges for this group'], 404);
         }
 
-        // Return the active challenge
+        // You might want to include userPermission in the response if it's needed on the front end
+        $activeChallenge['userPermission'] = $userPermission;
+
+        // Return the active challenge along with user permission
         return response()->json($activeChallenge, 200);
     }
 }
